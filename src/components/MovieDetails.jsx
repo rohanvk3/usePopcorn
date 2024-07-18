819a08a867752e98a4a383cd1d0e499fc8dd5b96
rohanvk3 <rohanvk003@gmail.com>
2024-07-18 19:23:44 +0530

    "changed http method"

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -60,7 +60,7 @@ export default function MovieDetails({
         try {
           setIsLoading(true);
           const res = await fetch(
-            `http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
+            `https://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
           );
           const data = await res.json();
           setMovie(data);
